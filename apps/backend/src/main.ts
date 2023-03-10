ed2bcca49037f28ef2daa9b7c9c0a486151d4061
Hallison <hallison.o.b@gmail.com>
2023-03-09 21:18:35 -0300

    init

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,20 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors())
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to backend!' });
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
